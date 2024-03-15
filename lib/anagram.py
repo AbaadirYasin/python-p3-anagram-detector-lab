5fc18856c6b1e56b47d20c48c744e71b1e5ada8c
AbaadirYasin <abaadir.yasin@student.moringaschool.com>
2024-03-15 11:41:14 +0000

    completed

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,14 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+
+    def match(self, possible_anagrams):
+        anagrams = []
+        for possible_anagram in possible_anagrams:
+            if self.is_anagram(possible_anagram):
+                anagrams.append(possible_anagram)
+        return anagrams
+
+    def is_anagram(self, possible_anagram):
+        return sorted(self.word) == sorted(possible_anagram.lower()) and self.word != possible_anagram.lower()
